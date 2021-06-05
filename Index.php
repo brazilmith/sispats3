8daf56ebcff37e73c7a319c3aa251034cbb8f6f7
Lucas Coelho <lucasdeacoelho@gmail.com>
2021-06-05 14:26:46 -0300

    Espelho do sispats2

--- Index.php
+++ Index.php
@@ -1,44 +1,46 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-	<head>
-		<!-- Meta tags Obrigatórias -->
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 
-		<title>Cadastro de Animais</title>
-		<link rel="stylesheet" type="text/css" href="css/estilo_menu.css"/>
+<head>
+	<!-- Meta tags Obrigatórias -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/estilo_menu.css" />
+	<title>Cadastro de Animais</title>
 
-	</head>
-	<body>
-		<img src="img/logo1.jpg"     width="12%" height="12%"/>
-		<img src="img/nome_loja.png" width="12%" height="12%"/>
-		<nav>
-			<ul class="menu">
-			    <li> <a href="index.php">DogsCats & Cia</a></li>
-				<li> <a href="#">Animais</a> </li>
-				<li> <a href="#">Donos</a>
-				<li> <a href="#">Cadastro Básico</a>
-					<ul class="submenu">
-						<li> <a href="lista_tipos.php">Tipos de Animais</a> </li>
-						<li> <a href="lista_raças.php">Raças de Animais</a> </li>
-						<li> <a href="lista_cores.php">Cores de Animais</a> </li>
-						<li> <a href="lista_cidades.php">Cidades</a> </li>
-						<li> <a href="lista_uf.php">UF</a> </li>
-					</ul>
-				</li>
-			</ul>
-		</nav>
 
-<button type="button" class="btn btn-primary">
-  Notificações <span class="badge badge-light">4</span>
-</button>
+</head>
 
+<body>
+	<img src="img/logo1.jpg" width="12%" height="12%" />
+	<img src="img/nome_loja.png" width="12%" height="12%" />
+	<nav>
+		<ul class="menu">
+			<li> <a href="index.php">DogsCats & Cia</a></li>
+			<li> <a href="#">Animais</a> </li>
+			<li> <a href="#">Donos</a>
+			<li> <a href="#">Cadastro Básico</a>
+				<ul class="submenu">
+					<li> <a href="lista_tipos.php">Tipos de Animais</a> </li>
+					<li> <a href="lista_raças.php">Raças de Animais</a> </li>
+					<li> <a href="lista_cores.php">Cores de Animais</a> </li>
+					<li> <a href="lista_cidades.php">Cidades</a> </li>
+					<li> <a href="lista_uf.php">UF</a> </li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+
+
+
+	
+	<a href="#" class="badge badge-success">Success</a>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	</body>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	< </body>
+
 </html>
-
-
-
--- Lista_Cores.php
+++ Lista_Cores.php
@@ -1,51 +1,67 @@
-<?php 
-	include('index.php');
-	include('Classe_Cor.php');
+<?php
+include('index.php');
+include('Classe_Cor.php');
 ?>
 <html>
-	<head>
-		<title>Lista de Cores de Animais</title>
-	</head>
-	<body>
-		<form name="formulario" action ="Incluir_cor.php" method="post">
-			<fieldset style="width:50%; margin:0 auto; margin-top:5%; position:center;">
-				<legend><strong>Lista de Cores de Animais</strong></legend>
-				<br>
-				<button type="submit" >Novo</button>
-				<a href="index.php">Voltar</a><br> 
-				<br>
-				<table>
-					<tr bgcolor = "#A9A9A9">
-						<td style = "text-align:center; width:3%; " >Código</td> 
-						<td style = "text-align:center; width:12%;" >Cor</td> 
-						<td style = "text-align:center; width:8%;" >Ações</td> 
-					<tr>
-					<?php 
-						$cor = new cor();  
+
+<head>
+	<title>Lista de Cores de Animais</title>
+</head>
+
+<body>
+	<form name="formulario" action="Incluir_cor.php" method="post">
+		<fieldset style="width:50%; margin:0 auto; margin-top:5%; position:center;">
+			<legend><strong>Lista de Cores de Animais</strong></legend>
+			<br>
+			<button type="submit" class="btn  btn-outline-dark" data-toggle="tooltip" data-placement="top" title="Adicionar Nova Cor">Cadastrar <span class="badge badge-light">+</span></button>
+			<a href="index.php" class=" btn-outline-dark btn-secondary btn-sm ">Voltar</a><br>
+			<br>
+
+
+			<nav>
+				<table class="table table-hover  table-bordered ">
+					<!--  -->
+					<thead>
+						<tr class="table-active">
+							<!---->
+							<td style="text-align:center; width:3%; ">Código</td>
+							<td style="text-align:center; width:12%;">Cor</td>
+							<td style="text-align:center; width:8%;">Ações</td>
+						<tr>
+					</thead>
+					<tbody>
+						<?php
+						$cor = new cor();
+						$result = 0;
 						$lista = $cor->selectAllCor();
-						while ( $registro = $lista->fetch_array() ){
-							echo '<tr>'; 
-							echo '    <td style="text-align:center;">'. $registro["CodigoCor"].'</td>';
-							echo '    <td style="text-align:left;">'  . $registro["DescricaoCor"].'</td>';
+						while ($registro = $lista->fetch_array()) {
+							echo '<tr> ';
+							echo '    <td class="table-active" style="text-align:center;">' . $registro["CodigoCor"] . '</td>';
+							echo '    <td style="text-align:center;">'  . $registro["DescricaoCor"] . '</td>';
 							echo '    <td style="text-align:center;">';
-							echo '         <a href="alterar_cor.php?id='.$registro["CodigoCor"].'">Editar</a>';
-							echo '         <a href="javascript:confirmacao(' .$registro["CodigoCor"].')">Excluir</a>';
+							echo '         <a class="btn-outline-info btn-sm" href="alterar_cor.php?id=' . $registro["CodigoCor"] . '">Editar</a>';
+							echo '         <a class="btn-outline-info btn-sm"href="javascript:confirmacao(' . $registro["CodigoCor"] . ') ">Excluir</a>';
 							echo '    </td>';
-							echo '</tr>'; 
+							echo '</tr>';
+							$result++;
 						}
-					?>			 
+						echo $result;
+						?></tbody>
 				</table>
-			</fieldset>
-		</form>
-	</body>
+			</nav>
+
+		</fieldset>
+	</form>
+</body>
+
 </html>
 
 
 <script language="Javascript">
-			function confirmacao(id) {
-     		var resposta = confirm("Você confirma a exclusão da cor " + id + " ?");
- 		     if (resposta == true) {
-          		window.location.href = "delete_Cor.php?id="+id;
-     			}	
-			}
+	function confirmacao(id) {
+		var resposta = confirm("Você confirma a exclusão da cor " + id + " ?");
+		if (resposta == true) {
+			window.location.href = "delete_Cor.php?id=" + id;
+		}
+	}
 </script>

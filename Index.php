54f0a47aab5f73d4a5a3f29f58d2474d21f407a0
Lucas Coelho <lucasdeacoelho@gmail.com>
2021-06-10 23:40:18 -0300

    alterando index.php

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/estilo_menu.css" />
+	<!--<link rel="stylesheet" type="text/css" href="css/estilo_menu.css" />-->
 	<title>Cadastro de Animais</title>
 
 
@@ -15,32 +15,45 @@
 <body>
 	<img src="img/logo1.jpg" width="12%" height="12%" />
 	<img src="img/nome_loja.png" width="12%" height="12%" />
-	<nav>
-		<ul class="menu">
-			<li> <a href="index.php">DogsCats & Cia</a></li>
-			<li> <a href="#">Animais</a> </li>
-			<li> <a href="#">Donos</a>
-			<li> <a href="#">Cadastro Básico</a>
-				<ul class="submenu">
-					<li> <a href="lista_tipos.php">Tipos de Animais</a> </li>
-					<li> <a href="lista_raças.php">Raças de Animais</a> </li>
-					<li> <a href="lista_cores.php">Cores de Animais</a> </li>
-					<li> <a href="lista_cidades.php">Cidades</a> </li>
-					<li> <a href="lista_uf.php">UF</a> </li>
-				</ul>
+
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<!-- Brand -->
+		<a class="navbar-brand" href="index.php">DogsCats & Cia</a>
+
+
+		<!-- Links -->
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Animais</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Donos</a>
+			</li>
+
+			<!-- Dropdown -->
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+					Cadastro Básico
+				</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="lista_tipos.php">Tipos de Animais</a>
+					<a class="dropdown-item" href="lista_raças.php">Raças de Animais</a>
+					<a class="dropdown-item" href="lista_cores.php">Cores de Animais</a>
+					<a class="dropdown-item" href="lista_cidades.php">Cidades</a>
+					<a class="dropdown-item" href="lista_uf.php">UF</a>
+
+
+				</div>
 			</li>
 		</ul>
-	</nav>
 
 
-
-	
-	<a href="#" class="badge badge-success">Success</a>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	< </body>
+		<a href="#" class="badge badge-success">Success</a>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		< </body>
 
 </html>

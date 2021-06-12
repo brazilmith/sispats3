2bbaa25bc38fa0b932933027c989c57f19048257
Lucas Coelho <lucasdeacoelho@gmail.com>
2021-06-11 23:30:54 -0300

    sim

--- Index_copy.php
+++ Index_copy.php
@@ -7,7 +7,6 @@ include_once('menu_principal.php');
 
 <head>
 
-
 	<title>Cadastro de Animais</title>
 
 </head>
@@ -16,70 +15,31 @@ include_once('menu_principal.php');
 
 	<!----///////////////////CAROUSEL///////////////////////----->
 	<div class="slideshow-container">
-
 		<div class="mySlides fade">
 			<div class="numbertext">1 / 3</div>
 			<img src="https://afolhatorres.com.br/wp-content/uploads/2020/07/pag-18-CAES-E-GATOS.jpg" style="width:100%">
 			<div class="text">Caption Text</div>
 		</div>
-
 		<div class="mySlides fade">
 			<div class="numbertext">2 / 3</div>
 			<img src="https://nutricaoesaudeanimal.com.br/wp-content/uploads/2020/08/VACCINAR-630x450.png" style="width:100%">
 			<div class="text">Caption Two</div>
 		</div>
-
 		<div class="mySlides fade">
 			<div class="numbertext">3 / 3</div>
 			<img src="https://blogs.correiobraziliense.com.br/maisbichos/wp-content/uploads/sites/7/2018/02/c%C3%A3o-e-gato-juntos.jpg" style="width:100%">
 			<div class="text">Caption Three</div>
 		</div>
-
 		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 		<a class="next" onclick="plusSlides(1)">&#10095;</a>
-
 	</div>
 	<br>
-
 	<div style="text-align:center">
 		<span class="dot" onclick="currentSlide(1)"></span>
 		<span class="dot" onclick="currentSlide(2)"></span>
 		<span class="dot" onclick="currentSlide(3)"></span>
 	</div>
 
-	<script>
-		var slideIndex = 1;
-		showSlides(slideIndex);
-
-		function plusSlides(n) {
-			showSlides(slideIndex += n);
-		}
-
-		function currentSlide(n) {
-			showSlides(slideIndex = n);
-		}
-
-		function showSlides(n) {
-			var i;
-			var slides = document.getElementsByClassName("mySlides");
-			var dots = document.getElementsByClassName("dot");
-			if (n > slides.length) {
-				slideIndex = 1
-			}
-			if (n < 1) {
-				slideIndex = slides.length
-			}
-			for (i = 0; i < slides.length; i++) {
-				slides[i].style.display = "none";
-			}
-			for (i = 0; i < dots.length; i++) {
-				dots[i].className = dots[i].className.replace(" active", "");
-			}
-			slides[slideIndex - 1].style.display = "block";
-			dots[slideIndex - 1].className += " active";
-		}
-	</script>
-	<!----/////////////////////////////////////----->
 
 	<!---------------------------BOOTSTRAP-------------------->
 
@@ -102,15 +62,18 @@ include_once('menu_principal.php');
 	<!------------------>
 	<div class="jumbotron">
 		<div class="container text-center">
-			<h1>My Portfolio</h1>
-			<p>Some text that represents "Me"...</p>
+			<h1>Meu Portfolio</h1>
+			<p>Mais textos pra pôr aqui...</p>
 		</div>
 	</div>
-	<!---------------------------FOOTER-------------------->
+
+	<!--------------------FOOTER-------------------->
 	<footer class="container-fluid text-center btn btn-secondary">
 		<p>Trabalho de PHP LTDA</p>
 	</footer>
-	<!---------------------------BOOTSTRAP-------------------->
+	<!----------------------------------------------->
+	<!----/////////////Importar comportamentos do boostrap////////////////////////----->
+	<script src="js/comportamentos.js"></script>
 </body>
 
 </html>
--- menu_principal.php
+++ menu_principal.php
@@ -76,6 +76,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!---------------------->
+    <script src="js/comportamentos.js"></script>
 </body>
 
 </html>
